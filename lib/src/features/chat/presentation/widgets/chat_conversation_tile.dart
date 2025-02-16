391578b2e791e11228596469e07e9f5262a6314f
Vishal Govind <vishal.govind2098@gmail.com>
2025-02-16 23:45:45 +0530

    add inline citations

--- lib/src/features/chat/presentation/widgets/chat_conversation_tile.dart
+++ lib/src/features/chat/presentation/widgets/chat_conversation_tile.dart
@@ -6,6 +6,7 @@ import 'package:flutter_markdown/flutter_markdown.dart';
 
 import 'package:ask_chuck/src/features/chat/models/session_conversation_api/response.dart';
 import 'package:ask_chuck/src/features/chat/presentation/widgets/sources_list_view.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ChatConversationTile extends StatefulWidget {
   ChatConversationTile.fromSessionConversation({
@@ -45,8 +46,48 @@ class _ChatConversationTileState extends State<ChatConversationTile> {
     super.initState();
   }
 
+  String _replaceSourcesWithMarkdownLinks(String text) {
+    Map<String, String?> sourceLinks = getSourceLinks();
+
+    // Use a RegExp to find all [SourceX] tags
+    RegExp exp = RegExp(r'\[Source(\d+)\]');
+
+    // Replace each [SourceX] tag with a Markdown link
+    return text.replaceAllMapped(exp, (Match match) {
+      final sourceKey = 'Source${match.group(1)}';
+      final url = sourceLinks[sourceKey];
+
+      if (url != null) {
+        return '[[${match.group(1)}]]($url)'; // Create Markdown link
+      } else {
+        return match.group(0)!; // Return original tag if URL not found
+      }
+    });
+  }
+
+  Map<String, String?> getSourceLinks() {
+    final Map<String, String?> sourceLinks = {};
+    for (int i = 0; i < widget.chatContext.length; i++) {
+      final ctx = widget.chatContext[i];
+      final (source, page) = (ctx.metaData?.source, ctx.metaData?.page);
+      sourceLinks["Source${i + 1}"] =
+          "$source${page != null ? "#page=$page" : ""}";
+    }
+    return sourceLinks;
+  }
+
+  Map<String, ChatContext> get indexedSources {
+    final Map<String, ChatContext> sourceLinks = {};
+    for (int i = 0; i < widget.chatContext.length; i++) {
+      final ctx = widget.chatContext[i];
+      sourceLinks["[${i + 1}]"] = ctx;
+    }
+    return sourceLinks;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final aiMessage = _replaceSourcesWithMarkdownLinks(widget.aiMessage);
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 16.0),
       child: Column(
@@ -65,8 +106,9 @@ class _ChatConversationTileState extends State<ChatConversationTile> {
             Markdown(
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
-              data: widget.aiMessage,
+              data: aiMessage,
               styleSheetTheme: MarkdownStyleSheetBaseTheme.material,
+              onTapLink: onTapLink,
             )
           else
             StreamingMessageBuilder(
@@ -76,18 +118,32 @@ class _ChatConversationTileState extends State<ChatConversationTile> {
                 });
               },
               key: ValueKey(widget.conversationId),
-              words: widget.aiMessage,
+              words: aiMessage,
               messageBuilder: (context, message) {
                 return Markdown(
                   shrinkWrap: true,
                   physics: const NeverScrollableScrollPhysics(),
                   data: message,
                   styleSheetTheme: MarkdownStyleSheetBaseTheme.material,
+                  onTapLink: onTapLink,
                 );
               },
             ),
         ],
       ),
     );
+  }
+
+  void onTapLink(String text, String? url, String? title) {
+    if (url != null) {
+      final ctx = indexedSources[text];
+      if (ctx != null) {
+        final (source, page) = (ctx.metaData?.source, ctx.metaData?.page);
+        final uri = Uri.parse(
+          "$source${page != null ? "#page=$page" : ""}",
+        );
+        launchUrl(uri);
+      }
+    }
   }
 }
